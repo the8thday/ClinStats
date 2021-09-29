d9978f7ea3c6189bc877fb6a8d90d6eeb37468de
congliu <dibatian@live.com>
2021-09-29 16:42:53 +0800

    add ci

--- R/CI.R
+++ R/CI.R
@@ -1,14 +1,28 @@
 # CI value for different hypo test
 
-ci_median <- function(){
-  DescTools
+ci_median <- function(x){
+  if(!is.vector(x)){
+    stop('only one dim vector surported!')
+  }
+  value <- DescTools::MeanCI(x)
+  as.data.frame(value) %>% t() %>%
+    tibble::as_tibble()
   }
 
 
 ci_mean <- function(x){
   if(!is.vector(x)){
-    print('only one dim vector surported!')
+    stop('only one dim vector surported!')
   }
-  DescTools::MeanCI(x)
+  value <- DescTools::MeanCI(x)
+  as.data.frame(value) %>% t() %>%
+    tibble::as_tibble()
 }
+
+
+ci_portation <-  function(x){
+  DescTools::BinomCI()
+}
+
+
 
