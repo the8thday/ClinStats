62d1579820e648ab91823a48087040bd6b7d8f4e
CongLiu <dibatian@live.com>
2022-06-07 14:53:51 +0800

    data.R

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' df is a dataset for test.
+#'
+#' A dataset containing three category variable and one continue variable
+#'
+#' @format A data frame with 96 rows and 4 variables:
+#' \describe{
+#'   \item{density}{different density}
+#'   \item{block}{different block}
+#'   \item{fertilizer}{fertilizer}
+#'   \item{yield}{outcome}
+#'   ...
+#' }
+#' @source \url{}
+"df"
+
+
+
+#' ancova_data is a test dataset for ANCOVA.
+#'
+#' A dataset containing three variables for ANCOVA
+#'
+#' @format A data frame with 76 rows and 3 variables:
+#' \describe{
+#'   \item{group}{group information}
+#'   \item{W1D1}{covariable}
+#'   \item{diffW24}{outcome}
+#'   ...
+#' }
+#' @source \url{}
+"ancova_data"
+
